5af623d595aa95cc196a3cfc017b0d69caaa94f7
Luis0624 <luis.apgo12@gmail.com>
2021-07-08 19:11:51 -0400

    first commit, working image carousel with 2 image displayed, after this will play with adding modals or buttons from react bootstrap without breaking it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import Carousel from "./components/Carousel/Carousel"
+// import logo from './logo.svg';
 import './App.css';
+import MainImg from './Images/mainImage.png'
+import GifImg from './Images/giphy.gif'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxWidth: 1200, marginLeft: 'auto', marginRight: 'auto', marginTop: 64 }}>
+     
+      <Carousel show ={2}>
+      <div style={{padding: 8}}>
+        <img
+          alt="img 1"
+          src={ GifImg }
+          style={{width: '100%'}}
+          // width="1600"
+          height="300"
+          // className="d-inline-block align-top"
+        />
+      </div>
+
+      <div style={{padding: 8}}>
+        <img
+          alt="img 2"
+          src={ MainImg }
+          style={{width: '100%'}}
+          // width="1600"
+          height="300"
+          // className="d-inline-block align-top"
+        />
+      </div>
+       
+      <div style={{padding: 8}}>
+        <img src="https://via.placeholder.com/1600x300" alt="placeholder" style={{width: '100%'}}/>
+      </div>
+
+      <div style={{padding: 8}}>
+        <img src="https://via.placeholder.com/1600x300" alt="placeholder" style={{width: '100%'}}/>
+      </div>
+
+      </Carousel>
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,95 @@
+import { React, useState, useEffect } from 'react'
+import './carousel.css'
+
+const Carousel = (props) => {
+    const {children, show} = props
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [length, setLength] = useState(children.length)
+    
+    // Set the length to match current children from props
+    useEffect(() => {
+        setLength(children.length)
+    }, [children])
+
+    // function to handle when the control button is clicked.
+    const next = () => {
+        if (currentIndex < (length - show)) {
+            setCurrentIndex(prevState => prevState + 1)
+        }
+    }
+    
+    const prev = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(prevState => prevState - 1)
+        }
+    }
+
+    // for touchscreens, swiping left/right to move images
+    const [touchPosition, setTouchPosition] = useState(null)
+
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX
+        setTouchPosition(touchDown)
+
+    }
+
+    const handleTouchMove = (e) => {
+         const touchDown = touchPosition
+        
+        if(touchDown === null) {
+            return
+        }
+        
+        const currentTouch = e.touches[0].clientX
+        const diff = touchDown - currentTouch
+        
+        if (diff > 5) {
+            next()
+        }
+        
+        if (diff < -5) {
+            prev()
+        }  
+
+        setTouchPosition(null)
+    }    
+
+
+    return (
+        <div className="carousel-container">
+
+            <div className="carousel-wrapper">
+
+                {   currentIndex > 0 &&
+                    <button onClick={prev} className="left-arrow">
+                        &lt;
+                    </button>
+                }
+
+                <div 
+                    className="carousel-content-wrapper"
+                    onTouchStart={handleTouchStart}
+                    onTouchMove={handleTouchMove}
+                >
+
+                    <div 
+                        className={`carousel-content show-${show}`}
+                        style={{ transform: `translateX(-${currentIndex * (100 / show)}%)` }}
+                    >
+                        {children}
+                    </div>
+
+                   {
+                        currentIndex < (length - show) &&
+                        <button onClick={next} className="right-arrow">
+                            &gt;
+                        </button>
+                    }
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Carousel
